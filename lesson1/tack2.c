a5112ed2d9430fce6079db06ef2f95594c7d07b1
Mikhail Pavlovskiy <149257280+mikhailpavlovskiy@users.noreply.github.com>
2023-10-29 22:34:50 +0300

    HW1 tasks

---
+++ lesson1/tack2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Let's\n");
+    printf("\tgo\n");
+    printf("\t\twalk\n");
+
+    return 0;
+}
---
+++ lesson1/task4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf(" _   _\n");
+    printf("/ \\_/ \\\n");
+    printf("\\_/ \\_/\n");
+    printf("/ \\_/ \\\n");
+    printf("\\_/ \\_/\n");
+
+    return 0;
+}
